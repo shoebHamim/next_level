c62b2a80be55dccacc54683ff006a38eeff5db04
Hamim <shoeb.hamim@gmail.com>
2026-01-10 20:53:01 +0600

    neon db connection

--- node_express/m12_express_0/src/server.ts
+++ node_express/m12_express_0/src/server.ts
@@ -1,25 +1,67 @@
-import express, { Request, Response } from 'express'
-const app = express()
-const port = 3000
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+import 'dotenv/config';
+const app = express();
+const port = 3000;
 // json parser middleware
-app.use(express.json())
+app.use(express.json());
 // form data middleware
 // app.use(express.urlencoded)
+const databasePassword=process.env.DB_PASSWORD
+const databaseUsername=process.env.DB_USER_NAME
+const pool = new Pool({
+  connectionString:
+    `postgresql://neondb_owner:${databasePassword}@${databaseUsername}.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+});
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!!!')
-})
 
-app.post('/',(req:Request,res:Response)=>{
+console.log(process.env.DB_PASSWORD);
+
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT  NULL,
+    age INT,
+    phone VARCHAR(20),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `);
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS todos(
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) on DELETE CASCADE,
+    title varchar(200) NOT NULL,
+    description TEXT,
+    is_completed BOOLEAN default false,
+    priority INT,
+    due_date DATE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )`);
+
+  const tables=await pool.query(`SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'public';`)
+console.log(tables);
+};
+initDB();
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!!!");
+});
+
+app.post("/", (req: Request, res: Response) => {
   // console.log(req);
   console.log(req.body);
   res.status(201).json({
-    success:true
-  })
-  
-})
-
+    success: true,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
