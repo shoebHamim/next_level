f6abe39cadfe702ed63547c94feb653ac7e8f9d7
“Shoeb” <shoeb.hamim@gmail.com>
2026-08-04 14:48:46 +0600

    setup nodmailer email verification and send demo email

--- prisma-pro-nextjs-ninja/m23-24-requirement-analysis-erd-project-setup-auth/src/lib/auth.ts
+++ prisma-pro-nextjs-ninja/m23-24-requirement-analysis-erd-project-setup-auth/src/lib/auth.ts
@@ -1,6 +1,18 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma.config";
+import nodemailer from "nodemailer";
+
+// Create a transporter using SMTP
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -21,17 +33,26 @@ export const auth = betterAuth({
   },
   emailAndPassword: {
     enabled: true,
-    requireEmailVerification: true, 
-    autoSignIn:false,
+    requireEmailVerification: true,
+    autoSignIn: false,
   },
-  emailVerification:{
-    sendVerificationEmail: async ( { user, url, token }, request) => {
-         // void sendEmail({
-         //   to: user.email,
-         //   subject: "Verify your email address",
-         //   text: `Click the link to verify your email: ${url}`,
-         // });
-         console.log('verification email sent!');
-       },
-  }
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      try {
+        const info = await transporter.sendMail({
+          from: '"Next Level Team" <shoeb@gmail.com>', // sender address
+          to: `${user.email}`, // list of recipients
+          subject: "You are registered!", // subject line
+          text: "Hello world?", // plain text body
+          html: "<b>Hello world?</b>", // HTML body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Preview URL is only available when using an Ethereal test account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      } catch (err) {
+        console.error("Error while sending mail:", err);
+      }
+    },
+  },
 });
